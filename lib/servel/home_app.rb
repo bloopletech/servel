0645206c79367f06e37fa669cd48b3aee140afce
Brenton "B-Train" Fletcher <i@bloople.net>
2018-03-20 00:50:16 +1100

    Return 404 for favicon request

--- lib/servel/home_app.rb
+++ lib/servel/home_app.rb
@@ -1,9 +1,12 @@
 class Servel::HomeApp
+  FAVICON_PATH = "/favicon.ico"
+
   def initialize(roots)
     @roots = roots
   end
 
   def call(env)
+    return [404, {}, []] if env["PATH_INFO"] == FAVICON_PATH
     Servel::HamlContext.render('home.haml', { roots: @roots })
   end
 end
